e0a86f74a8d11ea07deb6fbe5b6326e9494ec8b8
maxime.odye <maxime.odye@zenika.com>
2017-06-05 11:21:58 +0200

    feat: initial commit for the project

---
+++ src/main/java/org/zenika/matinale/es/loggenerator/LogGeneratorApplication.java
@@ -0,0 +1,12 @@
+package org.zenika.matinale.es.loggenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogGeneratorApplication.class, args);
+	}
+}
